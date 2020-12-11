9109c411fde733a53b4e8802993998c9f69d2051
Yuanlong Xiao <yuanlongxiao24@gmail.com>
2020-12-11 16:21:13 -0500

    decompose rendering OK. Before deleting other files

---
+++ input_src/rendering/operators/coloringFB_bot_m.h
@@ -0,0 +1,5 @@
+// color the frame buffer
+void coloringFB_bot_m(
+		hls::stream<ap_uint<32> > & Input_1,
+		hls::stream<ap_uint<32> > & Output_1);
+
---
+++ input_src/rendering/operators/coloringFB_top_m.h
@@ -0,0 +1,6 @@
+// color the frame buffer
+void coloringFB_top_m(
+		hls::stream<ap_uint<32> > & Input_1,
+		hls::stream<ap_uint<32> > & Input_2,
+		hls::stream<ap_uint<32> > & Output_1);
+
