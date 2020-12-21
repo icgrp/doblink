bca55e561d3a2fad48618de1082da46180ea495b
Yuanlong Xiao <yuanlongxiao24@gmail.com>
2020-12-20 21:04:08 -0500

    data_redir seems OK

--- input_src/rendering/operators/data_redir_m.cpp
+++ input_src/rendering/operators/data_redir_m.cpp
@@ -591,7 +591,117 @@ void data_redir_m (
   }
 
   //projection (input_lo,input_mi,input_hi,&triangle_2ds_1);
-  rasterization1 (triangle_2d, Output_1, Output_2);
+  //rasterization1 (triangle_2d, Output_1, Output_2);
 
+
+
+	Triangle_2D triangle_2d_same;
+	static bit8 max_min[5]={0, 0, 0, 0, 0};
+	static bit16 max_index[1]={0};
+	bit32 tmp1, tmp2, tmp3, tmp4;
+	static int parity = 0;
+
+#pragma HLS INLINE off
+// clockwise the vertices of input 2d triangle
+if ( check_clockwise( triangle_2d ) == 0 ){
+
+	tmp1(7,0) = 1;
+	tmp1(15, 8) = triangle_2d_same.x0;
+	tmp1(23,16) = triangle_2d_same.y0;
+	tmp1(31,24) = triangle_2d_same.x1;
+
+	tmp2(7,0) = triangle_2d_same.y1;
+	tmp2(15, 8) = triangle_2d_same.x2;
+	tmp2(23,16) = triangle_2d_same.y2;
+	tmp2(31,24) = triangle_2d_same.z;
+
+	tmp3(15,0) = max_index[0];
+	tmp3(23,16) = max_min[0];
+	tmp3(31,24) = max_min[1];
+
+	tmp4(7,0) = max_min[2];
+	tmp4(15, 8) = max_min[3];
+	tmp4(23,16) = max_min[4];
+	tmp4(31,24) = 0;
+	if(parity==0){
+		Output_1.write(tmp1);
+		Output_1.write(tmp2);
+		Output_1.write(tmp3);
+		Output_1.write(tmp4);
+		parity = 1;
+	}else{
+		Output_2.write(tmp1);
+		Output_2.write(tmp2);
+		Output_2.write(tmp3);
+		Output_2.write(tmp4);
+		parity = 0;
+	}
+#ifdef PROFILE
+data_redir_m_out_1+=4;
+#endif
+
+  return;
+}
+if ( check_clockwise( triangle_2d ) < 0 )
+  clockwise_vertices( &triangle_2d );
+
+
+
+
+// copy the same 2D triangle
+triangle_2d_same.x0 = triangle_2d.x0;
+triangle_2d_same.y0 = triangle_2d.y0;
+triangle_2d_same.x1 = triangle_2d.x1;
+triangle_2d_same.y1 = triangle_2d.y1;
+triangle_2d_same.x2 = triangle_2d.x2;
+triangle_2d_same.y2 = triangle_2d.y2;
+triangle_2d_same.z  = triangle_2d.z ;
+
+// find the rectangle bounds of 2D triangles
+max_min[0] = find_min( triangle_2d.x0, triangle_2d.x1, triangle_2d.x2 );
+max_min[1] = find_max( triangle_2d.x0, triangle_2d.x1, triangle_2d.x2 );
+max_min[2] = find_min( triangle_2d.y0, triangle_2d.y1, triangle_2d.y2 );
+max_min[3] = find_max( triangle_2d.y0, triangle_2d.y1, triangle_2d.y2 );
+max_min[4] = max_min[1] - max_min[0];
+
+// calculate index for searching pixels
+max_index[0] = (max_min[1] - max_min[0]) * (max_min[3] - max_min[2]);
+
+tmp1(7,0) = 0;
+tmp1(15,8) = triangle_2d_same.x0;
+tmp1(23,16) = triangle_2d_same.y0;
+tmp1(31,24) = triangle_2d_same.x1;
+
+tmp2(7,0) = triangle_2d_same.y1;
+tmp2(15,8) = triangle_2d_same.x2;
+tmp2(23,16) = triangle_2d_same.y2;
+tmp2(31,24) = triangle_2d_same.z;
+
+tmp3(15,0) = max_index[0];
+tmp3(23,16) = max_min[0];
+tmp3(31,24) = max_min[1];
+
+tmp4(7,0) = max_min[2];
+tmp4(15,8) = max_min[3];
+tmp4(23, 16) = max_min[4];
+tmp4(31, 24) = 0;
+
+if(parity==0){
+	Output_1.write(tmp1);
+	Output_1.write(tmp2);
+	Output_1.write(tmp3);
+	Output_1.write(tmp4);
+	parity = 1;
+}else{
+	Output_2.write(tmp1);
+	Output_2.write(tmp2);
+	Output_2.write(tmp3);
+	Output_2.write(tmp4);
+	parity = 0;
+}
+#ifdef PROFILE
+data_redir_m_out_1+=4;
+#endif
+return;
 }
 
