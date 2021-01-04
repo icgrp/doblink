b6b0ad1046b88f94aaf7e59624ea161faeebe222
Yuanlong Xiao <yuanlongxiao24@gmail.com>
2021-01-04 18:29:57 -0500

    color_top

--- input_src/rendering/operators/coloringFB_bot_m.h
+++ input_src/rendering/operators/coloringFB_bot_m.h
@@ -3,4 +3,4 @@ void coloringFB_bot_m(
 		hls::stream<ap_uint<32> > & Input_1,
 		hls::stream<ap_uint<32> > & Output_1);
 
-#pragma map_target = riscv page_num = 8 inst_mem_size = 262144
+#pragma map_target = HW page_num = 8 inst_mem_size = 262144
