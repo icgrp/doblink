0e558b3d21930790ac0cae26a903f7810b26cdeb
Andrew Butt <andrewb1999@gmail.com>
2021-01-04 16:44:38 -0500

    Start trying to get symbiflow to work

--- controller/rvpld-soc/nexys_video.py
+++ controller/rvpld-soc/nexys_video.py
@@ -43,25 +43,25 @@ class _CRG(Module):
         self.clock_domains.cd_sys4x     = ClockDomain(reset_less=True)
         self.clock_domains.cd_sys4x_dqs = ClockDomain(reset_less=True)
         self.clock_domains.cd_clk200    = ClockDomain()
-        self.clock_domains.cd_clk100    = ClockDomain()
+        #self.clock_domains.cd_clk100    = ClockDomain()
 
         # # #
 
-        self.submodules.pll = pll = S7MMCM(speedgrade=-1)
+        self.submodules.pll = pll = S7PLL(speedgrade=-1)
         self.comb += pll.reset.eq(~platform.request("cpu_reset"))
         pll.register_clkin(platform.request("clk100"), 100e6)
         pll.create_clkout(self.cd_sys,       sys_clk_freq)
         pll.create_clkout(self.cd_sys4x,     4*sys_clk_freq)
         pll.create_clkout(self.cd_sys4x_dqs, 4*sys_clk_freq, phase=90)
         pll.create_clkout(self.cd_clk200,    200e6)
-        pll.create_clkout(self.cd_clk100,    100e6)
+        #pll.create_clkout(self.cd_clk100,    100e6)
         self.submodules.idelayctrl = S7IDELAYCTRL(self.cd_clk200)
 
 # BaseSoC ------------------------------------------------------------------------------------------
 
 class BaseSoC(RvpldSoCCore):
     def __init__(self, sys_clk_freq=int(100e6), with_ethernet=False, **kwargs):
-        platform = nexys_video.Platform()
+        platform = nexys_video.Platform("symbiflow")
 
         # SoCCore ----------------------------------------------------------------------------------
         RvpldSoCCore.__init__(self, platform, sys_clk_freq,
