3babc3827ea611accfaf2a16c870a1a16c4ebde8
ept221 <ept221@lehigh.edu>
2021-06-22 23:20:42 -0400

    updated comments

--- do_blink/floorplanner/translator.py
+++ do_blink/floorplanner/translator.py
@@ -4,7 +4,7 @@ import copy
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 ###############################################################################
-# Read the yaml file
+# First we read the yaml file
 yaml_data = None
 with open("draft.yaml", 'r') as yaml_file:
     try:
@@ -14,7 +14,7 @@ with open("draft.yaml", 'r') as yaml_file:
         print(exc)
 
 ###############################################################################
-# First we get the interface information which is common to all pblocks
+# Next we get the interface information which is common to all pblocks
 
 # Here we get the clocks
 clocks = []
@@ -82,7 +82,7 @@ for pblock in yaml_data["overlay"]["pblocks"]["pblocks"]:
     interface_list = pblock_clocks + pblock_ports
     definition_dict["ports"] = interface_list
 
-    # We then add our new definition to the definitions list
+    # Finally we add our new definition to the definitions list
     definitions.append(definition_dict)
 ###############################################################################
 # Now we can print the json definitions for each pblock:
@@ -90,7 +90,7 @@ for definition in definitions:
     print(json.dumps(definition,indent=1))
     print()
 ###############################################################################
-# Plots a single rectangle 
+# This function plots a single rectangle 
 def plot_rect(axis, x_min, x_max, y_min, y_max, color, fill=False):
     rect = patches.Rectangle((x_min,y_min),
                               x_max - x_min,
@@ -101,8 +101,8 @@ def plot_rect(axis, x_min, x_max, y_min, y_max, color, fill=False):
     axis.add_patch(rect)
     return rect
 ###############################################################################
-# Plots a set of labeled rectangles, given a list of objects
-# from the yaml file
+# This function plots a set of labeled rectangles, given
+# a list of objects from the yaml file
 def plot_rects(axis, lst, color, label_color="black"):
     for element in lst:
         rect = plot_rect(axis,element["x_min"],element["x_max"],
@@ -124,7 +124,9 @@ def plot_rects(axis, lst, color, label_color="black"):
                     ha = "center",
                     va = "center")
 ###############################################################################
-# Setup the plot
+# Now we can plot our results
+
+# First we setup the plot
 ax = plt.gca()
 plt.xlim([-10, yaml_data["overlay"]["max_x_dimension"] + 10])
 plt.ylim([-10, yaml_data["overlay"]["max_y_dimension"] + 10])
@@ -133,12 +135,12 @@ ax.set_aspect('equal')
 plt.title(yaml_data["overlay"]["name"])
 plt.xlabel("Part: " + yaml_data["overlay"]["part"])
 
-# Plot the BFT, static blocks, and pblocks
+# Now we plot the BFT, static blocks, and pblocks
 plot_rects(ax, yaml_data["overlay"]["BFT"], "darkred")
 plot_rects(ax, yaml_data["overlay"]["static_blocks"], "darkgoldenrod")
 plot_rects(ax, yaml_data["overlay"]["pblocks"]["pblocks"], "teal")
 
-# Plot an outline of the chip
+# Finally we plot an outline of the chip
 plot_rect(ax, 0, yaml_data["overlay"]["max_x_dimension"],
               0, yaml_data["overlay"]["max_y_dimension"],
               color = "black")
